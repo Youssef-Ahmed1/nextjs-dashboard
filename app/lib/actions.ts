8390d226dd3f648d1a2359fd5b8d3d409ee591e8
Youssef Ahmed <moropass1212@gmail.com>
2026-06-20 21:20:11 +0300

    fixed a small bug

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -103,6 +103,6 @@ export async function deleteInvoice(id: string) {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath("/dashboard/invoices");
     } catch (error) {
-        return { message: "Database Error: Failed to Delete Invoice." };
+        throw new Error("Database Error: Failed to Delete Invoice.");
     }
 }
